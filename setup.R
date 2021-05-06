abedbe0b58e3485c606bffd2a4213b800e51c683
Ryan Romard <rromard@gmail.com>
2021-05-06 17:51:17 -0400

    First time pruning widgets

--- setup.R
+++ setup.R
@@ -7,18 +7,26 @@ library(rstudioapi)
 library(blogdown)
 library(here)
 
-# Workaround to create new page, set WD to empty temp directory
-setwd(here('temp_folder'))
-setwd('C:/Users/Ryan/Documents/rromard_website')
-new_site(theme = "wowchemy/starter-academic", force = TRUE)
+# # Workaround to create new page, set WD to empty temp directory
+# setwd(here('temp_folder'))
+# setwd('C:/Users/Ryan/Documents/rromard_website')
+# new_site(theme = "wowchemy/starter", force = TRUE)
 
 # will save settings, opens if exists, creates if new
 blogdown::config_Rprofile()
 
+# Change base URL
+rstudioapi::navigateToFile("config.yaml", line = 3)
 
+# Configs
+config_netlify()
 
-blogdown::new_post(title = "Hi Hugo", 
-                   ext = '.Rmarkdown', 
-                   subdir = "post")
-stop_server()
-serve_site()
+# Checks
+check_content()
+check_netlify()
+check_hugo()
+
+# Change to author/bio section
+navigateToFile("content/authors/admin/_index.md")
+# Changes to active widgets
+navigateToFile("content/home/accomplishments.md")
